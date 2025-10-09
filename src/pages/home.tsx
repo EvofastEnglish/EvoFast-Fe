5ef2c77af65b8127300d242b2af1c5205cd6fa89
ThanhTrong29 <203505393+ThanhTrong29@users.noreply.github.com>
2025-10-09 22:33:48 +0700

    feat(@app): update version app

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -39,7 +39,7 @@ const Home: React.FC = () => {
   );
 
   const getData = useCallback(async () => {
-    localStorageService.set<string>(version_app, "1.0.2");
+    localStorageService.set<string>(version_app, "1.0.3");
     await dispatch(getDataAiTests()).then(({ payload }) => {
       const data = payload as AiTestResult;
       localStorageService.set<AiTestResult>(DATA_AI_TEST, data);
